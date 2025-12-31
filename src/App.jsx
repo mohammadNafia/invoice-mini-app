6c0628b8bb72756e4b49d235b73b9311f9d201d9
MOHAMMED N. NADHEM <mohammadnafia1@gmail.com>
2025-12-31 21:34:13 +0300

    Add handelCaptureScreen to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
 import InvoicePage from "./components/InvoicePage.jsx";
 import Hero from "./components/hero.jsx";
 function App() {
-  const handleScan = () => {
-    my.scan({
-      type: 'qr',
-      success: (res) => {
-        my.alert({ title: res.code });
-      },
+  const handelCaptureScreen = () => {
+  Page({
+  data: {
+    condition: false,
+  },
+  onReady() {
+    my.onUserCaptureScreen(() => {
+      my.alert({
+        content: 'Received user screen capture',
+      });
     });
+  },
+  offUserCaptureScreen() {
+    my.offUserCaptureScreen();
+    this.setData({
+      condition: false,
+    });
+  },
+  onUserCaptureScreen() {
+    my.onUserCaptureScreen(() => {
+      my.alert({
+        content: 'Received user screen capture'
+      });
+    });
+    this.setData({
+      condition: true,
+    });
+  },
+});
   }
   return (
     <>
